c8abd55f9a8f3db1ec10684ea53dd1ca42b71528
Michael <michael.haar@gmx.at>
2017-07-25 13:08:02 +0200

    Pep8 compliant version

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,18 +3,18 @@ import fresh_tomatoes
 
 # create some movie instances
 forrest_gump = media.Movie("Forrest Gump",
-                           "https://images-na.ssl-images-amazon.com/images/M/MV5BYThjM2MwZGMtMzg3Ny00NGRkLWE4M2EtYTBiNWMzOTY0YTI4XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SY1000_CR0,0,757,1000_AL_.jpg",
+                           "https://images-na.ssl-images-amazon.com/images/M/MV5BYThjM2MwZGMtMzg3Ny00NGRkLWE4M2EtYTBiNWMzOTY0YTI4XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SY1000_CR0,0,757,1000_AL_.jpg",  # NOQA
                            "https://www.youtube.com/watch?v=uPIEn0M8su0")
 avengers = media.Movie("Avengers",
-                       "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk2NTI1MTU4N15BMl5BanBnXkFtZTcwODg0OTY0Nw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+                       "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk2NTI1MTU4N15BMl5BanBnXkFtZTcwODg0OTY0Nw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",  # NOQA
                        "https://www.youtube.com/watch?v=eOrNdBpGMv8")
 into_the_wild = media.Movie("Into the Wild",
-                            "https://images-na.ssl-images-amazon.com/images/M/MV5BMTAwNDEyODU1MjheQTJeQWpwZ15BbWU2MDc3NDQwNw@@._V1_.jpg",
+                            "https://images-na.ssl-images-amazon.com/images/M/MV5BMTAwNDEyODU1MjheQTJeQWpwZ15BbWU2MDc3NDQwNw@@._V1_.jpg",  # NOQA
                             "https://www.youtube.com/watch?v=2LAuzT_x8Ek")
 captain_fantastic = media.Movie("Captain Fantastic",
-                                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjE5OTM0OTY5NF5BMl5BanBnXkFtZTgwMDcxOTQ3ODE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+                                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjE5OTM0OTY5NF5BMl5BanBnXkFtZTgwMDcxOTQ3ODE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",  # NOQA
                                 "https://www.youtube.com/watch?v=D1kH4OMIOMc")
 
 # group the movie instances in a list
 movies = [forrest_gump, avengers, into_the_wild, captain_fantastic]
-fresh_tomatoes.open_movies_page(movies)     #this line does the magic and shows the website
+fresh_tomatoes.open_movies_page(movies)     # this line does the magic and shows the website
